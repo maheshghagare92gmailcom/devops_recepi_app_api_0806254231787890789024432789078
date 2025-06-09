cdbc70c2ca838599ab7b8734be8d07d085058f42
mahesh ghagare <maheshghagare92@gmail.com>
2025-06-09 16:02:46 +0530

    updated static media and static volumes new

--- proxy/run.sh
+++ proxy/run.sh
@@ -2,5 +2,9 @@
 
 set -e
 
-envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
-nginx -g 'daemon off;'
+python manage.py wait_for_db
+python manage.py collectstatic --noinput
+python manage.py migrate
+
+gunicorn --bind :9000 --workers 4 app.wsgi
+
