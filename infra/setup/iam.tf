f815ae430b535573f7709da106954684b45bdfab
mahesh ghagare <maheshghagare92@gmail.com>
2025-06-11 12:48:37 +0530

    new commit 12_48

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -17,7 +17,7 @@ resource "aws_iam_access_key" "cd" {
 data "aws_iam_policy_document" "tf_backend" {
   statement {
     effect    = "Allow"
-    actions   = ["s3:ListBucket"]
+    actions   = ["s3:ListBucket", "S3:ListObjectsV2"]
     resources = ["*"]
   }
 
