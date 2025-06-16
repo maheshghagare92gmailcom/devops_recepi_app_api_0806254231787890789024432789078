bc019a1c08d15b526d2052e5ff574fe956061461
mahesh ghagare <maheshghagare92@gmail.com>
2025-06-16 21:52:10 +0530

    16-6-9-52

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -203,7 +203,7 @@ resource "aws_ecs_service" "api" {
     security_groups = [aws_security_group.ecs_service.id]
   }
 
-   load_balancer {
+  load_balancer {
     target_group_arn = aws_lb_target_group.api.arn
     container_name   = "proxy"
     container_port   = 8000
