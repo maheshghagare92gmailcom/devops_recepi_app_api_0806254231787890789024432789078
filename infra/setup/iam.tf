88c6c2115d0b9642e4fc0b047738d4fdc3a46414
mahesh ghagare <maheshghagare92@gmail.com>
2025-06-21 16:00:46 +0530

    21-6-4-0

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -133,7 +133,8 @@ data "aws_iam_policy_document" "ec2" {
       "ec2:CreateInternetGateway",
       "ec2:AttachInternetGateway",
       "ec2:ModifyVpcAttribute",
-      "ec2:RevokeSecurityGroupIngress",
+      "ec2:DescribeAccountAttributes",
+      "ec2:RevokeSecurityGroupIngress"
     ]
     resources = ["*"]
   }
